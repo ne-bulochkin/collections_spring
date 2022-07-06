b80adf729201cffc62ccb5a5017dc7b111a875fd
BULANOV Andrey V <andrey.bulanov@raiffeisen.ru>
2022-07-06 03:06:29 +0300

    Initial commit

---
+++ src/main/java/ne/bulochkin/collections_spring/CollectionsSpringApplication.java
@@ -0,0 +1,13 @@
+package ne.bulochkin.collections_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionsSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollectionsSpringApplication.class, args);
+    }
+
+}
